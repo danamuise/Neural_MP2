7dd373a5fe7c7f4770b4249ad54c8b7fbb6a66ec
Dana Muise <Dana Muise>
2017-06-12 07:39:07 -0700

    added hard timer to scene 5

---
+++ MoviePlayer/Assets/Scripts/LoadScene.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LoadScene : MonoBehaviour {
+
+
+	public int sceneIndex;
+
+	// Use this for initialization
+	void Start () {
+		CameraFade.StartAlphaFade (Color.black, true, 3f, 0f, () => {
+
+		});
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void GoToMain(){
+		CameraFade.StartAlphaFade(Color.black, false, 1f, 0f, () =>
+			{
+                Debug.Log("Opening Scene");
+				SceneManager.LoadScene(0);
+			});
+
+	}
+
+    public void GoToScene(int i)
+    {
+        
+        CameraFade.StartAlphaFade(Color.black, false, 1f, 0f, () =>
+        {
+            Debug.Log("Opening Scene: " + i);
+            SceneManager.LoadScene(i);
+        });
+
+    }
+
+    public void OpenSceneWithFader () {
+		CameraFade.StartAlphaFade(Color.black, false, 1f, 0f, () =>
+			{
+				SceneManager.LoadScene(sceneIndex);
+			});
+	}
+}
